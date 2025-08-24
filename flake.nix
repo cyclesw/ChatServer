5598221b878bf88e544e10a767768a125f73ad15
cyclesw <1939653182@qq.com>
2025-08-24 17:17:36 +0800

    更新flake nix 结构

--- flake.nix
+++ flake.nix
@@ -1,83 +1,45 @@
 {
-  description = "A Nix-flake-based C/C++ development environment";
+  description = "GateWayServer C/C++ dev env (nix flake)";
 
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
 
-  outputs = { self, nixpkgs, }:
+  outputs = { self, nixpkgs }:
     let
-      project = "GateWayServer";
-
-      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
-      forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
-        pkgs = import nixpkgs { inherit system; };
-      });
-
-      brpc = pkgs: pkgs.callPackage ./thirdparty/brpc.nix {};
-
-      cpprestsdk = pkgs: pkgs.callPackage ./thirdparty/cpprestsdk.nix {};
-      etcd-cpp-apiv3 = pkgs: pkgs.callPackage ./thirdparty/etcd-cpp-api.nix {
-        cpprestsdk = cpprestsdk pkgs;
-      };
-      odb = pkgs: pkgs.callPackage ./thirdparty/odb.nix {};
-      libodb = pkgs: pkgs.callPackage ./thirdparty/libodb.nix {};
-      libodb-mysql = pkgs: pkgs.callPackage ./thirdparty/libodb-mysql.nix {
-        libodb = libodb pkgs;
-      };
-      libodb-boost = pkgs: pkgs.callPackage ./thirdparty/libodb-boost.nix {};
-      libmysqlclient = pkgs: pkgs.callPackage ./thirdparty/libmysqlclient.nix {};
-   in
+      systems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      eachSystem = f: nixpkgs.lib.genAttrs systems (s: f (import nixpkgs {
+        inherit s;
+        overlays = [ self.overlays.default ];
+      }));
+    in
     {
-      devShells = forEachSupportedSystem ({ pkgs }: {
-        default = pkgs.mkShell.override
-          {
-            # Override stdenv in order to change compiler:
-          #  stdenv = pkgs.clangStdenv;
-          }
-          {
-            packages = with pkgs; [
-              # env
-              llvmPackages_latest.clang
-              llvmPackages_latest.libllvm
-              llvmPackages_latest.libcxx
-              llvmPackages_latest.lldb
-              llvmPackages_latest.libstdcxxClang
-              clang-tools
-              gcc  # for odb compiler
+      # ---------- overlay：把第三方包一次性注入 nixpkgs ----------
+      overlays.default = final: prev: {
+        brpc          = final.callPackage ./thirdparty/brpc.nix          { };
+        cpprestsdk    = final.callPackage ./thirdparty/cpprestsdk.nix    { };
+        etcd-cpp-apiv3= final.callPackage ./thirdparty/etcd-cpp-api.nix  { cpprestsdk = final.cpprestsdk; };
+        odb           = final.callPackage ./thirdparty/odb.nix           { };
+        libodb        = final.callPackage ./thirdparty/libodb.nix        { };
+        libodb-mysql  = final.callPackage ./thirdparty/libodb-mysql.nix  { libodb = final.libodb; };
+        libodb-boost  = final.callPackage ./thirdparty/libodb-boost.nix  { };
+        libmysqlclient= final.callPackage ./thirdparty/libmysqlclient.nix{ };
+      };
 
-              cmake
-              pkg-config
-              ninja
-              (odb pkgs)
-
-              protols
-
-              websocketpp
-              spdlog
-              asio_1_10
-              protobuf
-              redis-plus-plus
-              httplib
-              jsoncpp
-              gflags
-              gtest
-              
-              (libodb pkgs)
-              (libodb-mysql pkgs)
-              (libodb-boost pkgs)
-              (brpc pkgs)
-              (etcd-cpp-apiv3 pkgs)
-              (cpprestsdk pkgs)
-              (libmysqlclient pkgs)
-
-              curl
-              leveldb
-              abseil-cpp
-              openssl
-              snappy
-              zstd
-           ];
-          };
-      });
+      # ---------- devShell ----------
+      devShells = eachSystem (pkgs:
+        pkgs.mkShell.override { stdenv = pkgs.clangStdenv; } {
+          packages = with pkgs; [
+            # 编译&调试
+            clang-tools pkg-config cmake 
+            # 运行时 / 第三方库
+            brpc cpprestsdk etcd-cpp-apiv3
+            libodb libodb-mysql libodb-boost odb
+            libmysqlclient libamqpcpp libev
+            # 常用系统库
+            protobuf redis-plus-plus httplib jsoncpp
+            gflags gtest websocketpp spdlog asio_1_10
+            # 隐形依赖
+            curl leveldb abseil-cpp openssl snappy zstd
+          ];
+        });
     };
 }
-
