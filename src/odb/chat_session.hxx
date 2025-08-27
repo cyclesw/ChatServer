9449d5dac5e2e09d78071dc92bfc4f65f1a1303e
cyclesw <1939653182@qq.com>
2025-08-27 14:44:58 +0000

    格式化odb-cpp文档

--- src/odb/chat_session.hxx
+++ src/odb/chat_session.hxx
@@ -1,66 +1,90 @@
 #pragma once
-#include <string>
 #include <odb/forward.hxx>
+#include <string>
 #include "chat_session_member.hxx"
 
-namespace im {
 
-enum class ChatSessionType {
-    SINGLE = 1,
-    GROUP = 2
-};
+namespace im
+{
+
+    enum class ChatSessionType
+    {
+        SINGLE = 1,
+        GROUP = 2
+    };
 
 #pragma db object table("chat_session")
-class ChatSession {
+    class ChatSession
+    {
     public:
-        ChatSession(){}
-        ChatSession(const std::string &ssid,
-            const std::string &ssname, const ChatSessionType sstype):
-            _chat_session_id(ssid),
-            _chat_session_name(ssname),
-            _chat_session_type(sstype){}
+        ChatSession()
+        {
+        }
+        ChatSession(const std::string &ssid, const std::string &ssname, const ChatSessionType sstype) :
+            _chat_session_id(ssid), _chat_session_name(ssname), _chat_session_type(sstype)
+        {
+        }
 
-        std::string chat_session_id() const { return _chat_session_id; }
-        void chat_session_id(std::string &ssid)  { _chat_session_id = ssid; }
+        std::string chat_session_id() const
+        {
+            return _chat_session_id;
+        }
+        void chat_session_id(std::string &ssid)
+        {
+            _chat_session_id = ssid;
+        }
 
-        std::string chat_session_name() const { return _chat_session_name; }
-        void chat_session_name(std::string &ssname)  { _chat_session_name = ssname; }
+        std::string chat_session_name() const
+        {
+            return _chat_session_name;
+        }
+        void chat_session_name(std::string &ssname)
+        {
+            _chat_session_name = ssname;
+        }
 
-        ChatSessionType chat_session_type() const { return _chat_session_type; }
-        void chat_session_type(ChatSessionType val) { _chat_session_type = val; }
+        ChatSessionType chat_session_type() const
+        {
+            return _chat_session_type;
+        }
+        void chat_session_type(ChatSessionType val)
+        {
+            _chat_session_type = val;
+        }
+
     private:
         friend class odb::access;
-        #pragma db id auto
+#pragma db id auto
         unsigned long _id;
-        #pragma db type("varchar(64)") index unique
+#pragma db type("varchar(64)") index unique
         std::string _chat_session_id;
-        #pragma db type("varchar(64)")
+#pragma db type("varchar(64)")
         std::string _chat_session_name;
-        #pragma db type("tinyint")
-        ChatSessionType _chat_session_type; //1-单聊； 2-群聊
-};
+#pragma db type("tinyint")
+        ChatSessionType _chat_session_type; // 1-单聊； 2-群聊
+    };
 
 // 这里条件必须是指定条件：  css::chat_session_type==1 && csm1.user_id=uid && csm2.user_id != csm1.user_id
-#pragma db view object(ChatSession = css)\
-                object(ChatSessionMember = csm1 : css::_chat_session_id == csm1::_session_id)\
-                object(ChatSessionMember = csm2 : css::_chat_session_id == csm2::_session_id)\
-                query((?))
-struct SingleChatSession {
-    #pragma db column(css::_chat_session_id)
-    std::string chat_session_id;
-    #pragma db column(csm2::_user_id)
-    std::string friend_id;
-};
+#pragma db view object(ChatSession = css)                                                                              \
+        object(ChatSessionMember = csm1 : css::_chat_session_id == csm1::_session_id)                                  \
+                object(ChatSessionMember = csm2 : css::_chat_session_id == csm2::_session_id) query((?))
+    struct SingleChatSession
+    {
+#pragma db column(css::_chat_session_id)
+        std::string chat_session_id;
+#pragma db column(csm2::_user_id)
+        std::string friend_id;
+    };
 
 // 这里条件必须是指定条件：  css::chat_session_type==2 && csm.user_id=uid
-#pragma db view object(ChatSession = css)\
-                object(ChatSessionMember = csm : css::_chat_session_id == csm::_session_id)\
-                query((?))
-struct GroupChatSession {
-    #pragma db column(css::_chat_session_id)
-    std::string chat_session_id;
-    #pragma db column(css::_chat_session_name)
-    std::string chat_session_name;
-};
+#pragma db view object(ChatSession = css) object(ChatSessionMember = csm : css::_chat_session_id == csm::_session_id)  \
+        query((?))
+    struct GroupChatSession
+    {
+#pragma db column(css::_chat_session_id)
+        std::string chat_session_id;
+#pragma db column(css::_chat_session_name)
+        std::string chat_session_name;
+    };
 
-}
+} // namespace im
--- src/odb/user.hxx
+++ src/odb/user.hxx
@@ -5,106 +5,107 @@
 #ifndef CHATSERVER_USER_HXX
 #define CHATSERVER_USER_HXX
 
-#include <odb/nullable.hxx>
 #include <odb/forward.hxx>
+#include <odb/nullable.hxx>
 #include <string>
+
 
 namespace im
 {
 #pragma db object table("user")
-class User
-{
-public:
-    User() = default;
-    // 用户名--新增用户 -- 用户ID, 昵称，密码
-    User(const std::string &uid, const std::string &nickname, const std::string &password)
-        : _user_id(uid), _nickname(nickname), _password(password)
+    class User
     {
-    }
-    // 手机号--新增用户 -- 用户ID, 手机号, 随机昵称
-    User(const std::string &uid, const std::string &phone) : _user_id(uid), _nickname(uid), _phone(phone)
-    {
-    }
+    public:
+        User() = default;
+        // 用户名--新增用户 -- 用户ID, 昵称，密码
+        User(const std::string &uid, const std::string &nickname, const std::string &password) :
+            _user_id(uid), _nickname(nickname), _password(password)
+        {
+        }
+        // 手机号--新增用户 -- 用户ID, 手机号, 随机昵称
+        User(const std::string &uid, const std::string &phone) : _user_id(uid), _nickname(uid), _phone(phone)
+        {
+        }
 
-    void user_id(const std::string &val)
-    {
-        _user_id = val;
-    }
-    std::string user_id()
-    {
-        return _user_id;
-    }
+        void user_id(const std::string &val)
+        {
+            _user_id = val;
+        }
+        std::string user_id()
+        {
+            return _user_id;
+        }
 
-    std::string nickname()
-    {
-        if (_nickname)
-            return *_nickname;
-        return std::string();
-    }
-    void nickname(const std::string &val)
-    {
-        _nickname = val;
-    }
-
-    std::string description()
-    {
-        if (!_description)
+        std::string nickname()
+        {
+            if (_nickname)
+                return *_nickname;
             return std::string();
-        return *_description;
-    }
-    void description(const std::string &val)
-    {
-        _description = val;
-    }
+        }
+        void nickname(const std::string &val)
+        {
+            _nickname = val;
+        }
 
-    std::string password()
-    {
-        if (!_password)
-            return std::string();
-        return *_password;
-    }
-    void password(const std::string &val)
-    {
-        _password = val;
-    }
+        std::string description()
+        {
+            if (!_description)
+                return std::string();
+            return *_description;
+        }
+        void description(const std::string &val)
+        {
+            _description = val;
+        }
 
-    std::string phone()
-    {
-        if (!_phone)
-            return std::string();
-        return *_phone;
-    }
-    void phone(const std::string &val)
-    {
-        _phone = val;
-    }
+        std::string password()
+        {
+            if (!_password)
+                return std::string();
+            return *_password;
+        }
+        void password(const std::string &val)
+        {
+            _password = val;
+        }
 
-    std::string avatar_id()
-    {
-        if (!_avatar_id)
-            return std::string();
-        return *_avatar_id;
-    }
-    void avatar_id(const std::string &val)
-    {
-        _avatar_id = val;
-    }
+        std::string phone()
+        {
+            if (!_phone)
+                return std::string();
+            return *_phone;
+        }
+        void phone(const std::string &val)
+        {
+            _phone = val;
+        }
 
-private:
-    friend class odb::access;
+        std::string avatar_id()
+        {
+            if (!_avatar_id)
+                return std::string();
+            return *_avatar_id;
+        }
+        void avatar_id(const std::string &val)
+        {
+            _avatar_id = val;
+        }
+
+    private:
+        friend class odb::access;
 #pragma db id auto
-    unsigned long _id{};
+        unsigned long _id{};
 #pragma db type("varchar(64)") index unique
-    std::string _user_id;
+        std::string _user_id;
 #pragma db type("varchar(64)") index unique
-    odb::nullable<std::string> _nickname;
-    odb::nullable<std::string> _description;
+        odb::nullable<std::string> _nickname;
+        odb::nullable<std::string> _description;
 #pragma db type("varchar(64)")
-    odb::nullable<std::string> _password;
+        odb::nullable<std::string> _password;
 #pragma db type("varchar(64)") index unique
-    odb::nullable<std::string> _phone;
+        odb::nullable<std::string> _phone;
 #pragma db type("varchar(64)")
-    odb::nullable<std::string> _avatar_id;
-};
+        odb::nullable<std::string> _avatar_id;
+    };
 } // namespace im
 #endif // CHATSERVER_USER_HXX
